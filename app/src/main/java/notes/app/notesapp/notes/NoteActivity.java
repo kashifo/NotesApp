a5e1cc8ef6dfb8c1811b319db67a441756a16c5f
kashifo <koderkashif@gmail.com>
2017-06-07 14:55:03 +0530

    Added ability to receive data from other apps. Finished auto save feature.

--- app/src/main/java/notes/app/notesapp/notes/NoteActivity.java
+++ app/src/main/java/notes/app/notesapp/notes/NoteActivity.java
@@ -26,7 +26,8 @@ public class NoteActivity extends AppCompatActivity {
     DBHelper dbHelper;
     boolean isNew = true; //checks whether it's new note
     Note note; //current note
-    String oldData;
+    String oldData = ""; //to check data changes for auto saving
+    String prevData; //old data from existing note for revert purpose
     boolean save = true;
 
     @Override
@@ -42,27 +43,57 @@ public class NoteActivity extends AppCompatActivity {
         et_Note = (EditText) findViewById(R.id.etNote);
 
         dbHelper = new DBHelper(this);
-
-        if (getIntent().hasExtra("data")) {
-
-            isNew = false;
-            int noteId = getIntent().getExtras().getInt("noteId");
-            note = dbHelper.getNote(noteId);
-            et_Title.setText(note.getTitle());
-            et_Note.setText(note.getNote());
-            oldData = note.getNote();
-
-        } else {
-            note = new Note();
-        }
+        receiveData();
 
     }//onCreate
 
+    //checks and handle's note open mode
+    private void receiveData(){
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        //checks whther some other app is sending text data
+        if( action != null && action.equals(Intent.ACTION_SEND) ){
+            if( type != null && type.equals("text/plain") ){
+                String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+                if ( sharedText != null  && !sharedText.isEmpty() ) {
+                    // Update UI to reflect text being shared
+                    note = new Note();
+                    et_Note.setText( sharedText );
+                }
+            }
+        }else {
+            //check whether a note is opened from list
+            if ( getIntent().hasExtra("data") ) {
+
+                isNew = false;
+                int noteId = getIntent().getExtras().getInt("noteId");
+                note = dbHelper.getNote(noteId);
+                et_Title.setText(note.getTitle());
+                et_Note.setText(note.getNote());
+                prevData = note.getNote();
+
+            } else {
+                //note is not opened, create new note
+                note = new Note();
+            }
+        }
+
+    }
+
     @Override
     public void onPause() {
-        if ( save && !oldData.equals(et_Note.getText().toString())) {
-            saveNote();
+
+        String currentText = et_Note.getText().toString();
+
+        if( !currentText.isEmpty() && save ){
+            if( !oldData.equals(currentText) ){
+                saveNote();
+            }
         }
+
         super.onPause();
     }
 
@@ -84,6 +115,7 @@ public class NoteActivity extends AppCompatActivity {
                     inTitle = inNote.substring(0, inNote.length());
                 }
 
+                et_Title.setText( inTitle );
             }
 
             note.setTitle(inTitle);
@@ -100,8 +132,8 @@ public class NoteActivity extends AppCompatActivity {
 
 
             if (id != 0L) {
+                oldData = inNote;
                 Snackbar.make(rv_rootLayout, "Note Saved", Snackbar.LENGTH_SHORT).show();
-                finish();
             }
         } else {
             Snackbar.make(rv_rootLayout, "Empty note can't be saved", Snackbar.LENGTH_SHORT).show();
@@ -151,7 +183,7 @@ public class NoteActivity extends AppCompatActivity {
                         if (isNew)
                             et_Note.setText("");
                         else
-                            et_Note.setText(oldData);
+                            et_Note.setText(prevData);
 
                     }
                 })
@@ -166,7 +198,7 @@ public class NoteActivity extends AppCompatActivity {
 
     private void shareNote() {
         Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-        sharingIntent.setType("text/*");
+        sharingIntent.setType("text/plain");
         sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT,
                 et_Title.getText() + "\n" +
                         et_Note.getText()
